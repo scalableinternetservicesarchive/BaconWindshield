0131cdcdfcc216b84bae95a7b316a4a8f60fb648
samsoncs <samsoncs@stud.ntnu.no>
2014-11-25 12:24:57 -0800

    Added some fragment and http caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-  config.cache_store = :dalli_store
+  #config.cache_store = :dalli_store
 
   config.after_initialize do
     Bullet.enable = true
