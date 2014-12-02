0dfb45dc08f45fef1dd838a1a8d2770b371d4942
samsoncs <samsoncs@stud.ntnu.no>
2014-12-01 20:25:03 -0800

    quickfix

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -3,6 +3,7 @@ class LandingsController < ApplicationController
   
   def index
     result = request.location
+    
     if(result!=nil)
       #in dev and test environments, lat & long are 0.0
       if (result.latitude != 0.0 && result.longitude != 0.0)
@@ -12,8 +13,8 @@ class LandingsController < ApplicationController
 
         @nearbys = nearest.first(5)
 
-        allbest = nearest.sort_by { |a| (a.infos.last.swell_rating*(a.infos.last.size_max+a.infos.last.size_min)/2) }
-        @bestnearbys = allbest.last(5)
+        allbest = nearest.sort_by { |a| (-a.infos.last.swell_rating*(a.infos.last.size_max+a.infos.last.size_min)/2) }
+        @bestnearbys = allbest.first(5)
 
       else
         loc = Location.new(latitude: 34.42, longitude: -119.86)
@@ -21,8 +22,8 @@ class LandingsController < ApplicationController
 
         @nearbys = nearest.first(5)
 
-        allbest = nearest.sort_by { |a| (a.infos.last.swell_rating*(a.infos.last.size_max+a.infos.last.size_min)/2) }
-        @bestnearbys = allbest.last(5)
+        allbest = nearest.sort_by { |a| (-a.infos.last.swell_rating*(a.infos.last.size_max+a.infos.last.size_min)/2) }
+        @bestnearbys = allbest.first(5)
       end
     end
 
