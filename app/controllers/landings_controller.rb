5f5b0e79acc980febac619c4c245171f5b402a04
Anders <andersk.kristiansen@gmail.com>
2014-12-01 19:13:25 -0800

    option to switch to best nearby spots

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,37 +1,35 @@
 class LandingsController < ApplicationController
   def index
     result = request.location
-  
+
     #in dev and test environments, lat & long are 0.0
     if (result.latitude != 0.0 && result.latitude != 0.0)
       loc = Location.new(latitude: result.latitude, longitude: result.longitude)
-      nearest = loc.nearbys(50)
+      
+      # @nearbys = Location.near(loc, 50, select: "locations.*, infos.*").joins(:infos).first(5)
+
       @nearbys = nearest.first(5)
       
-      allbest = nearest
-      allbest.sort_by { |a| a.name }
-      @bestspots = allbest.first(5)
-      
+      allbest = nearest.sort_by { |a| (a.infos.last.swell_rating*(a.infos.last.size_max+a.infos.last.size_min)/2) }
+      @bestnearbys = allbest.last(5)
+
     else
       loc = Location.new(latitude: 34.42, longitude: -119.86)
       nearest = loc.nearbys(50)
+      
       @nearbys = nearest.first(5)
       
-      allbest = nearest
-      # allbest.sort_by { |a| a.infos.swell_rating }
-      @bestspots = allbest.first(5)
-      # @nearbys = Location.near(loc, 50, select: "locations.*, infos.*").joins(:infos).first(5)
+      allbest = nearest.sort_by { |a| (a.infos.last.swell_rating*(a.infos.last.size_max+a.infos.last.size_min)/2) }
+      @bestnearbys = allbest.last(5)
     end
-    
-    
-    
+
     if defined?(Devise)
       if user_signed_in?
         @favorites = current_user.favorites.limit(5)
       end
     end
-    
-    #fresh_when(etag: [@nearbys, @favorites, current_user])
+
+  #fresh_when(etag: [@nearbys, @favorites, current_user])
 
   end
 
