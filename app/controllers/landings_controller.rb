f829f282e1bd9c7f207091a913de6d43a6315728
samsoncs <samsoncs@stud.ntnu.no>
2014-12-01 18:28:48 -0800

    Update landings_controller.rb

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -3,10 +3,13 @@ class LandingsController < ApplicationController
   
   def index
     result = request.location
-    #in dev and test environments, lat & long are 0.0
-    if (result.latitude != 0.0 && result.latitude != 0.0)
-      loc = Location.new(latitude: result.latitude, longitude: result.longitude)
-      @nearbys = loc.nearbys(50).first(5)
+    if(result.latitude)
+      #in dev and test environments, lat & long are 0.0
+      if (result.latitude != 0.0 && result.latitude != 0.0)
+        loc = Location.new(latitude: result.latitude, longitude: result.longitude)
+        @nearbys = loc.nearbys(50).first(5)
+      end
+    end
       #@nearbys = Location.near(loc.name, 50, select: "locations.*, infos.*").joins(:infos)
       #@nearbys = Location.near(loc, 50, :select => "locations.*, infos.*").joins(:infos)
 
