060b997db34820aabe3bc47604e4ec0d5fdeade7
samsoncs <samsoncs@stud.ntnu.no>
2014-12-06 17:52:56 -0800

    afa

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -20,6 +20,7 @@ class LocationsController < ApplicationController
   end
 
   def show
+    @bool = false
     begin
       @waves = @location.infos.paginate(page: params[:page], per_page: 8, total_entries: 40)
       gon.watch.infos = @location.infos
@@ -29,7 +30,7 @@ class LocationsController < ApplicationController
     end
     @windValues1 = [[1, "Light air", "#58ACFA"], [2, "Light breeze", "#A9D0F5"], [3, "Gentle breeze", "#58FAF4"], [4, "Moderate breeze", "#9FF781"], [5, "Fresh breeze", "#F4FA58"], [6, "Strong breeze", "#F5D0A9"]] 
     @windValues2 = [[7, "High wind", "#F7BE81"], [8, "Gale", "#FE642E"], [9, "Strong gale", "#FF0000"], [10, "Storm", "#B43104"], [11, "Violent storm", "#B40404"], [12, "Hurricane force", "#8A0808"]]
-    fresh_when(etag: [@waves, @location, current_user])
+    #fresh_when(etag: [@waves, @location, current_user])
 
   end
 
