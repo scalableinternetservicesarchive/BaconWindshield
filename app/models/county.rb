7587a71643477f1fafb91e5bc4ecab89f36a52b8
samsoncs <samsoncs@stud.ntnu.no>
2014-12-12 21:17:18 -0800

    ak

--- app/models/county.rb
+++ app/models/county.rb
@@ -1,6 +1,6 @@
 class County < ActiveRecord::Base
   has_many :locations
   
-  reverse_geocoded_by :latitude, :longitude
-  after_validation :geocode
+  #reverse_geocoded_by :latitude, :longitude
+  #after_validation :geocode
 end
