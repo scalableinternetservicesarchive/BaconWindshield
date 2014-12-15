a96fd8838e3f27a259fe278f015719b4502b4dfb
Anders Kristiansen <anderskk@users.noreply.github.com>
2014-12-15 15:44:06 -0800

    Update landings_helper.rb

--- app/helpers/landings_helper.rb
+++ app/helpers/landings_helper.rb
@@ -2,7 +2,7 @@ module LandingsHelper
   def landings_init
     #begin
       #result = request.location
-      result = Location.first(20)
+      result = Location.first(30)
     #rescue
       #result = [0,0]
     #end
@@ -17,9 +17,9 @@ module LandingsHelper
         #@nearbys = nearest.first(5)
 
         @nearbys = result.first(5)
-        
+        allbest = result.sort_by { |a| (-a.infos.first.swell_rating*(a.infos.first.size_max + a.infos.first.size_min)/2) }
         #allbest = result.sort_by { |a| (-a.infos.last.swell_rating*(a.infos.first.size_max + a.infos.first.size_min)/2) }
-        @bestnearbys = result.last(5).sort_by { |a| (-a.infos.first.swell_rating*(a.infos.first.size_max + a.infos.first.size_min)/2) }
+        @bestnearbys = allbest.first(5)
       #else
         #loc = Location.new(latitude: 34.42, longitude: -119.86)
         #nearest = loc.nearbys(30)
